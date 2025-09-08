37d42256dcadc5abded07fb66666121f5e98b8ef
Kahjiitspy <artyrhamuliak@gmail.com>
2025-09-08 18:46:27 -0400

    Register and begin login

--- AngelAPI/Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ AngelAPI/Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43979fe032040d9f41cdbfbd60fa4a6c79a457eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7216223711989a80bec64ed603e074029dc3d3ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
