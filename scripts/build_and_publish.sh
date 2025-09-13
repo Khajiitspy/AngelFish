d3805f977479fd206effe3dca8337f80f309d5aa
novakvova <novakvova@gmail.com>
2025-09-13 19:01:00 +0300

    Working publish

--- scripts/build_and_publish.sh
+++ scripts/build_and_publish.sh
@@ -4,16 +4,14 @@ set -e  # зупиняє скрипт при помилці
 cd ../Angelfish
 docker build -t angelfish-client .
 docker login
-docker tag angelfish-client:latest magicabyss/angelfish-client:latest
-docker push magicabyss/angelfish-client:latest
+docker tag angelfish-client:latest novakvova/angelfish-client:latest
+docker push novakvova/angelfish-client:latest
 echo "Done ---client---!"
-
-cd ..
 
 cd ../AngelAPI
 docker build -t angelfish-api .
-docker tag angelfish-api:latest magicabyss/angelfish-api:latest
-docker push magicabyss/angelfish-api:latest
+docker tag angelfish-api:latest novakvova/angelfish-api:latest
+docker push novakvova/angelfish-api:latest
 echo "Done ---api---!"
 
 read -p "Press any key to exit..."
