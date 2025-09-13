16b23b67a6ed2797942968b5013dcacc9f48d025
Kahjiitspy <artyrhamuliak@gmail.com>
2025-09-13 11:44:30 -0400

    proper commit

---
+++ AngelAPI/AngelAPI/controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Core.Interfaces;
+using Core.Models.Account;
+using Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtbWebApi.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AccountController(IJwtTokenService jwtTokenService,
+    IMapper mapper, 
+    UserManager<UserEntity> userManager,
+    IImageService imageService) : ControllerBase
+{
+
+    [HttpPost]
+    public async Task<IActionResult> Login([FromForm] LoginModel model)
+    {
+        var user = await userManager.FindByEmailAsync(model.Email);
+        if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+        {
+            var token = await jwtTokenService.CreateTokenAsync(user);
+            return Ok(new { Token = token });
+        }
+        return Unauthorized("Invalid email or password");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register([FromForm] RegisterModel model)
+    {
+        var user = mapper.Map<UserEntity>(model);
+
+        user.Image = await imageService.SaveImageAsync(model.ImageFile!);
+
+        var result = await userManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
+        {
+            var token = await jwtTokenService.CreateTokenAsync(user);
+            return Ok(new
+            {
+                status = 200,
+                isValid = true,
+                message = "Registration successful",
+                userToken = token
+            });
+        }
+        else
+        {
+            return BadRequest(new
+            {
+                status = 400,
+                isValid = false,
+                errors = "Registration failed"
+            });
+        }
+
+    }
+}
---
+++ AngelAPI/AngelAPI/controllers/CategoriesController.cs
@@ -0,0 +1,75 @@
+﻿using Core.Interfaces;
+using Core.Models.Category;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngelAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController(ICategoriesService categoriesService) : Controller
+{
+    [HttpGet("list")]
+    public async Task<IActionResult> List()
+    {
+        var model = await categoriesService.GetAllAsync();
+
+        return Ok(model);
+    }
+
+    [HttpPost("create")]
+    //[Authorize(Roles = Roles.Admin)]
+    public async Task<IActionResult> Create([FromForm] CategoryCreateModel model)
+    {
+        var existingCategory = await categoriesService.GetBySlugAsync(model.Slug);
+
+        if (existingCategory != null)
+        {
+            return BadRequest(new
+            {
+                status = 400,
+                isValid = false,
+                errors = new[] { "Slug already exists on another category!" }
+            });
+        }
+        
+        var result = await categoriesService.CreateAsync(model);
+
+        return Ok(result);
+    }
+
+    [HttpPut("update")]
+    public async Task<IActionResult> Update([FromForm] CategoryEditModel model)
+    {
+        var existingCategory = await categoriesService.GetBySlugAsync(model.Slug);
+
+        if (existingCategory != null && existingCategory.Id != model.Id)
+        {
+            return BadRequest(new
+            {
+                status = 400,
+                isValid = false,
+                errors = new[] { "Slug already exists on another category!" }
+            });
+        }
+
+        var result = await categoriesService.UpdateAsync(model);
+        return Ok(result);
+    }
+
+    [HttpGet("{slug}")]
+    public async Task<IActionResult> GetBySlug(string slug)
+    {
+        var result = await categoriesService.GetBySlugAsync(slug);
+
+        return Ok(result);
+    }
+
+    [HttpDelete("delete")]
+    //[Authorize(Roles = Roles.Admin)]
+    public async Task<IActionResult> Delete([FromBody] CategoryDeleteModel model)
+    {
+        await categoriesService.DeleteAsync(model);
+        return Ok($"Category with id: {model.Id} deleted");
+    }
+}
